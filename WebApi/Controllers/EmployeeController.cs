6901e2aa90bfd9c788915a64c1e5138b26bec89b
shachar kaduri <shachar.kaduri@gmail.com>
2021-11-29 20:26:06 +0200

    commit project

---
+++ WebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,54 @@
+﻿using DATA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Dto;
+
+namespace WebApi.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        // GET api/<controller>
+        public List<EmployeeDto> Get()
+        {
+            MusicStoreDbContext db = new MusicStoreDbContext();
+            List<EmployeeDto> employee_dto = db.Employees.Select(x => new EmployeeDto()
+            {
+                EmployeeId = x.EmployeeId,
+                FullName = x.LastName + x.FirstName,
+                Title = x.Title,
+                City = x.City,
+                //MonthsWorked = (DateTime.Now.Month + DateTime.Now.Year * 12) - (x.HireDate.HasValue == true ? x.HireDate.Value.Year : -1  + (x.HireDate.HasValue == true ? x.HireDate.Value.Month : -1) * 12),
+                TotalTracksSold = db.InvoiceLines.Where(i => i.Invoice.Customer.EmployeeId == x.EmployeeId).Sum(i => i.Quantity),
+                TotalPrice = db.InvoiceLines.Where(i => i.Invoice.Customer.EmployeeId == x.EmployeeId).Sum(i => i.Quantity * i.UnitPrice)
+            }).OrderByDescending(c => c.TotalTracksSold).ToList();
+
+
+            return employee_dto;
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApi/Dto/EmployeeDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace WebApi.Dto
+{
+    public class EmployeeDto
+    {
+        public int EmployeeId { get; set; }
+        public string FullName { get; set; }
+        public string Title { get; set; }
+        public string City { get; set; }
+        public int MonthsWorked { get; set; }
+        public int TotalTracksSold { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
